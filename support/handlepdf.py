f1de7430365d7ce665fb13647253e3d10ca25690
Oscar Koeroo <okoeroo@gmail.com>
2025-05-16 07:45:15 +0200

    fix pdf when crashing

--- support/handlepdf.py
+++ support/handlepdf.py
@@ -1,11 +1,18 @@
 from PyPDF2 import PdfReader
+from PyPDF2.generic import NullObject
 from io import BytesIO
 from support.filter_support import find_exact_word
 
 
 def read_pdf_from_file(filepath: str) -> PdfReader:
-    reader = PdfReader(filepath)
+    try:
+        reader = PdfReader(filepath)
+    except Exception as e:
+        print(f"Error in PdfReader: \"{e}\" for \"{filepath}\"")
+        return None
+
     if reader.is_encrypted:
+        print(f"Warning: PDF is encrypted. \"{filepath}\"")
         return None
     return reader
 
@@ -49,7 +56,18 @@ def apply_pdf_filters_as_attachment(config: dict, context: dict, pdf_reader: Pdf
     keywords = config['keywords']
 
     for page in pdf_reader.pages:
-        text = page.extract_text()
+        # Extra check voor inhoudsobject
+        contents = page.get("/Contents")
+        if isinstance(contents, NullObject):
+            print(f"Warning: Pagina heeft geen inhoud, wordt overgeslagen.")
+            continue
+
+        # Checked
+        try:
+            text = page.extract_text()
+        except Exception as e:
+            print("Error: PDF problem on this page: {e}")
+            continue
 
         ### Matching
         for item in keywords:
